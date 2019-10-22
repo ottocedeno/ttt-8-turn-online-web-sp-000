282c0eceaf10a4361fa31fc71e1afdc085a339f5
Otto Cedeno <otto@ottostacos.com>
2019-10-22 22:34:30 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -38,9 +38,10 @@ end
 
 def turn(board)
   puts "Please enter 1-9:"
-  user_input = gets.chomp
 
+  user_input = gets.chomp
   index = input_to_index(user_input)
+
   if valid_move?(board, index)
     move(board, index)
     display_board(board)
